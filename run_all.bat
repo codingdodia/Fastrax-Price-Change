253fafde77025baf3471ea933943a36ab664cb77
codingdodia <dhairyadodia2024@gmail.com>
2025-08-27 16:04:57 -0500

    changing bat file to work better

--- run_all.bat
+++ run_all.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Start backend in a new cmd window
-start cmd /k "cd backend\src && call ..\..\venv\Scripts\activate && python main.py"
+start cmd /k "cd backend\src && call venv\Scripts\activate && python main.py"
 REM Start frontend in a new cmd window
 start cmd /k "cd frontend && npm run dev"
