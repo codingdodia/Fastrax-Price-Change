dcf9f7b90e536bffea8ceff55be3bf8bdd93098f
codingdodia <dhairyadodia2024@gmail.com>
2025-11-07 14:35:40 -0600

    Turned off sourcemnaps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build:{
+    sourcemap : false,
+  },
   plugins: [react()],
   server: {
     port: 5500,
